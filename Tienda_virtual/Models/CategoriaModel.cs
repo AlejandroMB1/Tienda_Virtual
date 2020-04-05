7f6659753fecc11d4f8b087c8ab38f062e0491ff
Alejandro Meza <alejo5123@hotmail.com>
2020-04-05 15:28:40 -0500

    Json e implementacion Rest

--- Tienda_virtual/Models/CategoriaModel.cs
+++ Tienda_virtual/Models/CategoriaModel.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using Tienda_virtual.Propagacion;
 using Tienda_virtual.Models;
+using Newtonsoft.Json;
 
 namespace Tienda_virtual.Models
 {
     public class CategoriaModel : NotificationObjectModel
     {
         #region Properties
-        public long ID { get; set; }
+        [JsonIgnore]
+        public long ID { get; set; } /*base de datos*/
+        [JsonProperty("idCategoria")]
+        public long IdCategoria { get; set; } /*API REST*/
+
+        [JsonProperty("nombreCategoria")]
         public String Categoria { get; set; }
+
+        [JsonIgnore]
         public QRModel QR { get; set; }
         #endregion Properties
         #region Initialize
